55f3031f94da1829f3c1d8d5e4d62818d3335697
Nuutti Varvikko <nvarvikko@gmail.com>
2023-08-07 02:10:47 +0300

    Check that correct number of arguments are passed

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
+import sys
+
+from util import fail
+
+
 def run():
-    pass
+    args = sys.argv[1:]
+
+    if len(args) != 2:
+        fail("Two JSON files are required")
+
 
 if __name__ == "__main__":
     run()
