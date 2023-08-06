06946afa3632a89556e4c27e9975c04ea38ee32e
Nuutti Varvikko <nvarvikko@gmail.com>
2023-08-07 02:05:47 +0300

    Add function for reading json files

--- util.py
+++ util.py
@@ -4,3 +4,10 @@ from sys import stderr, exit
 def fail(reason, exit_code=1):
     stderr.write(reason + "\n")
     exit(exit_code)
+
+
+def read_json(path):
+    try:
+        open(path, mode="r")
+    except OSError:
+        fail(f"{path}: no such file exists")
