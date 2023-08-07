fb97678e905afb313fae06dab7ecc0f405cdf1d4
Nuutti Varvikko <nvarvikko@gmail.com>
2023-08-07 04:03:08 +0300

    Finalize

--- main.py
+++ main.py
@@ -1,13 +1,40 @@
 import sys
 
-from util import fail
+from util import fail, read_json, write_csv, get_basename
 
 
 def run():
     args = sys.argv[1:]
+    output_path = None
+    json_paths = []
 
-    if len(args) != 2:
-        fail("Two JSON files are required")
+    while len(args) > 0:
+        arg = args.pop(0)
+
+        if arg in ["-o", "--output"]:
+            try:
+                output_path = args.pop(0)
+            except IndexError:
+                fail(f"""Path for the output file must be specified when using "{arg}" flag""")
+        else:
+            json_paths.append(arg)
+
+    # print(output_path)
+
+    # print(json_paths)
+    if len(json_paths) != 2:
+        fail("path to the JSON files are required")
+
+
+    source_dict = read_json(json_paths[0])
+    target_dict = read_json(json_paths[1])
+    value_pairs = []
+
+    for key, value in source_dict.items():
+        if key in target_dict:
+            value_pairs.append((value, target_dict[key]))
+
+    write_csv(output_path if output_path is not None else "_".join(map(get_basename, json_paths)) + "_aligned.csv", value_pairs)
 
 
 if __name__ == "__main__":
--- util.py
+++ util.py
@@ -1,13 +1,31 @@
+import os
 from sys import stderr, exit
+import json, csv
 
 
 def fail(reason, exit_code=1):
-    stderr.write(reason + "\n")
+    stderr.writelines([
+        f"Error -- {reason}\n",
+        "\nUsage: linguine <json-file> <json-file> [-o|--output <output-file>]\n"
+    ])
     exit(exit_code)
+
+
+def get_basename(path):
+    return os.path.splitext(os.path.basename(path))[0]
 
 
 def read_json(path):
     try:
-        open(path, mode="r")
+        return json.load(open(path, mode="r"))
     except OSError:
         fail(f"{path}: no such file exists")
+    except json.decoder.JSONDecodeError:
+        fail(f"{path}: invalid format")
+
+
+def write_csv(path, data):
+    file_obj = open(path, "w")
+    csv.writer(file_obj,
+               delimiter="\t"
+               ).writerows(data)
